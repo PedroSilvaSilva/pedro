b5779c4dd347b0df1bee21b819ff7e567e8c2f2b
Pedro Silva <119366212+PedroSilvaSilva@users.noreply.github.com>
2023-05-08 19:09:56 +0100

    feat: resetGlobalstyles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <GlobalStyle />
+    </div>
+  );
 }
 
 export default App;
